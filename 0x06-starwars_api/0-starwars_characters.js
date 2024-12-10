a2a7adb01389e1065177f9b8ea793883f390463b
Mr-wilz <juciwhilf@gmail.com>
2024-12-10 03:26:28 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 
-/**
- * This script fetches and displays chars in sta wars movie
- */
-
 const request = require('request');
-
-/**
- * Sends a request to the Star Wars API to get characters for the given movie ID
- * and logs each character's name in the specified order.
- *
- * @param {string} movieId - The ID of the movie to fetch characters from
- */
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
@@ -26,3 +15,4 @@ const exactOrder = (actors, x) => {
     exactOrder(actors, x + 1);
   });
 };
+localStorage
